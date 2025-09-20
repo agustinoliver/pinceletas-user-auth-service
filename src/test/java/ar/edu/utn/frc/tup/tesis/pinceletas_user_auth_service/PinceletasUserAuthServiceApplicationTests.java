c2dcba3b99f50025e5ea4253ea6bd9d770709afc
114112_Herrado_Tomas <114112@tecnicatura.frc.utn.edu.ar>
2025-09-20 00:53:50 -0300

    commit

---
+++ src/test/java/ar/edu/utn/frc/tup/tesis/pinceletas_user_auth_service/PinceletasUserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.tesis.pinceletas_user_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinceletasUserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
