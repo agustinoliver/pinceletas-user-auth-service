40dda480d004fc0330b0a52f2dcf33983bd113c6
114112_Herrado_Tomas <114112@tecnicatura.frc.utn.edu.ar>
2025-09-20 00:53:50 -0300

    commit

---
+++ src/main/java/ar/edu/utn/frc/tup/tesis/pinceletas_user_auth_service/PinceletasUserAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.tesis.pinceletas_user_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinceletasUserAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinceletasUserAuthServiceApplication.class, args);
+	}
+
+}
